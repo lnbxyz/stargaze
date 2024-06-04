8f61626c3c02368131ee126343bb5fa25ed20b09
nicole branco <hyaluna@gmail.com>
2024-06-04 18:18:32 -0300

    feat: update info description, add splashes

--- src/app/components/info/info.component.ts
+++ src/app/components/info/info.component.ts
@@ -7,12 +7,19 @@ import { Component, signal } from '@angular/core';
   styleUrl: 'info.component.scss',
 })
 export class InfoComponent {
-  headlines = [
+  options = [
     'is a venn diagram for stars',
     'helps you find crossovers',
     'is an actor spotting tool',
+    'for finding crossed paths',
+    'finds hidden connections',
+    'something about déjà vu',
+    'same faces, different places',
+    'hollywood is not that big',
+    'wait, i know them from...',
+    'something about kevin bacon',
   ];
-  headline = signal(
-    this.headlines[Math.floor(Math.random() * this.headlines.length)]
+  splash = signal(
+    this.options[Math.floor(Math.random() * this.options.length)]
   );
 }
